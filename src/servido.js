78c9643c293e2e4cf4535ec32f3ce0377bab0632
Jean Lucas <j3anlucas010@gmail.com>
2025-10-04 07:41:25 -0300

    feat: 🔥 servidor Node básico criado! Rotas de produto funcionando com sucesso

--- src/servido.js
+++ src/servido.js
@@ -1,10 +1,33 @@
 import http from "node:http"
 
-const server=http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+    const { url, method } = req
 
-return res.end('hello word !')
+
+    if (method === "GET" && url === "/produto") {
+        return res.end('Buscando Produto !')
+    }
+    if (method === "POST" && url === "/produto") {
+        return res.writeHead(201).end("✅ Tarefa criada com sucesso!")
+    }
+    if(method === "DELETE" && url === "/produto"){
+        return res.end(`O Produto de ID: Foi Deletado`)
+    }
+
+    return res.writeHead(404).end('❌ Rota não encontrada!')
+
+
+
+
+
+
+
+
+
 
 
 })
 
-server.listen(3333)
+server.listen(3333,()=>{
+     console.log("🚀 Servidor rodando em http://localhost:3333");
+})
